e922d1197e35cb3f80f151c7e6cc1f1b8baee894
Lan Stare <lan.stare42@gmail.com>
2025-03-26 20:12:08 +0100

    prvi poskus movementa z bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(movement)
+        .run();
 }
+
+fn movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    let mut transform = query.single_mut();
+
+    let speed = 2.0;
+    if keyboard_input.pressed(KeyCode::W) {
+        transform.translation.y += speed;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        transform.translation.y -= speed;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        transform.translation.x -= speed;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        transform.translation.x += speed;
+    }
+}
+
+#[derive(Component)]
+struct Player;
