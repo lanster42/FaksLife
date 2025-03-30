04f29a84119fdea49415dda5006006d77f35cc22
Lan Stare <lan.stare42@gmail.com>
2025-03-30 12:12:33 +0200

    discarded bevy game engine

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,5 @@
-use bevy::prelude::*;
+
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_system(movement)
-        .run();
+    println!("Hello world")
 }
-
-fn movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>,
-) {
-    let mut transform = query.single_mut();
-
-    let speed = 2.0;
-    if keyboard_input.pressed(KeyCode::W) {
-        transform.translation.y += speed;
-    }
-    if keyboard_input.pressed(KeyCode::S) {
-        transform.translation.y -= speed;
-    }
-    if keyboard_input.pressed(KeyCode::A) {
-        transform.translation.x -= speed;
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        transform.translation.x += speed;
-    }
-}
-
-#[derive(Component)]
-struct Player;
